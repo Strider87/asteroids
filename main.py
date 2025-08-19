fc470aacb46122968112df77c9139cec71fa97a1
Strider87 <sharatpatki@gmail.com>
2025-08-19 15:55:43 -0400

    Add forward and backward motion

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ from player import Player
 
 def main():
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    # player
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -24,8 +20,8 @@ def main():
         player.draw(screen)
         pygame.display.flip()
 
-        # divide by 1000 to convert into seconds
-        dt = (clock.tick(60)) / 1000
+        # limit FPS to 60
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
