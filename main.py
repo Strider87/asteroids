217b3fb23fa22b8144123cf812129e339f108413
Strider87 <sharatpatki@gmail.com>
2025-08-19 14:32:11 -0400

    Added the player and associated data

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     clock = pygame.time.Clock()
     dt = 0
@@ -14,6 +18,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0), rect=None, special_flags=0)
+        player.draw(screen)
+        
         pygame.display.flip()
 
         # divide by 1000 to convert into seconds
